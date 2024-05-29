dbd3ad8546809a49b6fab3d405deb9957498a4c5
shravanrawas <shravanrawas8@gmail.com>
2024-05-29 15:34:54 +0530

    new add

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -28,23 +28,23 @@ function Cart() {
          {
           products.length > 0 ? (
             <div className='container mx-auto h-auto grid grid-cols-5 gap-8'>
-            <div className='w-full h-full bg-white px-4 col-span-4'>
-                <div className='font-titleFont flex items-center justify-between border-b-[1px] border-b-gray-400 py-3'>
+            <div className='w-full bg-white px-4 col-span-5 xl:col-span-4'>
+                <div className='font-titleFont hidden xl:flex items-center justify-between border-b-[1px] border-b-gray-400 py-3'>
                     <h2 className='text-3xl font-medium'>Shopping cart</h2>
                     <h4 className='text-xl font-semmi-normal'>Subtitle</h4>
                 </div>
                 <div>
                     {
                         products.map((item) => (
-                          <div key={item.id} className='w-full border-b-[1px] border-b-gray-300 p-4 flex items-center gap-6'>
+                          <div key={item.id} className='w-full border-b-[1px] border-b-gray-300 p-4 md:p-0 md:py-4 flex items-center gap-6'>
 
-                            <div className='w-full flex items-center justify-between gap-6'>
-                            <div className='w-1/5'>
+                            <div className='w-full flex flex-col md:flex-row items-center gap-6'>
+                            <div className='w-full md:w-2/5 xl:w-1/5'>
                                <img className='w-full h-44 object-contain' src={item.image} alt="" />
                               </div>
-                              <div className='w-4/5'>
+                              <div className='w-full flex flex-col gap-2 xl:gap-1'>
                                 <h2 className='font-semibold text-lg'>{item.title}</h2>
-                                <p className='text-sm'>{item.description.slice(0,200)}</p>
+                                <p className='xl:pr-10 text-sm'>{item.description.slice(0,200)}</p>
                                 <p className='text-base'>Unit Price <span className='font-semibold'>${item.price}</span></p>
                                 <div className='bg-[#F0F2F2] flex justify-center items-center gap-1 w-24 py-1 text-center drop-shadow-lg rounded-md'>
                                      <p>Qty:</p>
@@ -54,7 +54,7 @@ function Cart() {
                                 </div>
                                 <button onClick={()=>dispatch(deleteitem(item.id))} className='bg-red-500 w-36 py-1 rounded-lg text-white mt-2 hover:bg-red-700 active:bg-red-900 duration-300'>Delete item</button>
                               </div>
-                              <div>
+                              <div className='w-full md:w-24'>
                                 <p className='text-lg font-titleFont font-semibold'>${item.price * item.quantity}</p>
                               </div>
                             </div>
@@ -68,14 +68,14 @@ function Cart() {
                   >Clear Cart</button>
                 </div>
             </div>
-            <div className='w-full h-52 bg-white col-span-1 flex flex-col justify-center items-center p-4'>
+            <div className='col-span-5 md:col-span-3 lg:col-span-2 xl:col-span-1 bg-white h-52 flex flex-col items-center p-4'>
                  <div>
-                   <p><span className='bg-white text-green-500 rounded-full'><CheckCircleIcon/></span>{''}
+                   <p className='flex gap-2 items-start text-sm'><span className='bg-white text-green-500 rounded-full'><CheckCircleIcon/></span>{''}
                    Your order qualifies for FREE Shipping....
                    </p>
                  </div>
                  <div className='font-semibold px-15 py-1 flex items-center gap-2 justify-between'>
-                  <p>Total: <span className='text-lg font-bold'>${totalprice}</span></p>
+                  <p className='font-semibold px-6 py-1 flex items-center justify-between'>Total: <span className='text-lg xl:w-24 font-titleFont font-semibold'> ${totalprice}</span></p>
                  </div>
                  <button class="w-full font-titleFont font-medium text-base bg-gradient-to-tr from-yellow-400 to-yellow-200 border hover:from-yellow-300 hover:to-yellow-400 border-yellow-500 hover:border-yellow-700 active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200 py-1.5 rounded-md mt-3">Proceed to Buy</button>
             </div>
